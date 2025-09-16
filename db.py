b12052647c2611bacb6e777d9612a7533d2c2a7c
Rohit Saundarmal <128357304+Rohiii-777@users.noreply.github.com>
2025-09-16 17:02:36 +0530

    fix: remove unnecessary connect_args from create_engine in db.py

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ import os
 DATABASE_URL = os.getenv("DATABASE_URL")
 # DATABASE_URL = "sqlite:///./db.sqlite"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
