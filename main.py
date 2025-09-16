0ddf9221920b7ce7982e1429e3384b75f2b4d5d8
Rohit Saundarmal <128357304+Rohiii-777@users.noreply.github.com>
2025-09-16 14:49:44 +0530

    fix: update CORS origins to include production frontend URL

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI(title="QnA Backend - FastAPI",debug=True)
 # Allow CORS for frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://interview-prep-frontend-kappa.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
